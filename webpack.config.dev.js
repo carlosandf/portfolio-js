d86aec9279dc91b61be1bc2cbb4eba1562157aab
carlosandf <carlos.andres.ff2912@gmail.com>
2022-04-11 19:16:05 -0500

    Utilice un archivo de configuracion de desarrollo para crear un build en modo development

---
+++ webpack.config.dev.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const copyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  // Entry nos permite decir el punto de entrada de nuestra aplicación
+  entry: "./src/index.js",
+  // Output nos permite decir hacia dónde va enviar lo que va a preparar webpacks
+  output: {
+    // path es donde estará la carpeta donde se guardará los archivos
+    // Con path.resolve podemos decir dónde va estar la carpeta y la ubicación del mismo
+    path: path.resolve(__dirname, "dev"),
+    // filename le pone el nombre al archivo final
+    filename: "[name].[contenthash].js",
+  },
+  mode: 'development',
+  resolve: {
+    // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
+    extensions: [".js"],
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+    }
+  },
+  module: {
+    /* ================ RULES ================ */
+    rules: [
+      {
+        // Test declara que extensión de archivos aplicara el loader
+        test: /\.m?js$/,
+        // Exclude permite omitir archivos o carpetas especificas
+        exclude: /node_modules/,
+        // Use es un arreglo u objeto donde dices que loader aplicaras
+        use: {
+          loader: "babel-loader"
+        }
+      },
+
+      // Rules for mini-css-extract-plugin
+      {
+        test: /\.css|.styl$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'stylus-loader',
+        ],
+      },
+
+      // Rules for images loader
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[hash][ext][query]',
+        }
+      },
+
+      // Rules for fonts
+      {
+        test: /\.(woff|woff2)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: './assets/fonts/[name][contenthash][ext]',
+        },
+      }
+    ]
+  },
+  /* ================ PLUGINS ================ */
+  plugins: [
+    /*
+      HtmlWebpackPlugin agrega las etiquetas
+      de los archivos de las que se necesitan
+      cargar en el documento HTML. Por ejemplo:
+      la etiqueta script y la etiqueta link:css
+    */
+    new HtmlWebpackPlugin({ // CONFIGURACIÓN DEL PLUGIN
+      inject: true, // INYECTA EL BUNDLE AL TEMPLATE HTML
+      template: './public/index.html', // LA RUTA AL TEMPLATE HTML
+      filename: './index.html' // NOMBRE FINAL DEL ARCHIVO
+    }),
+    new MiniCssExtractPlugin({
+      filename: './styles/[name].[contenthash].css'
+    }),
+    new copyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
+        }
+      ]
+    }),
+    new Dotenv(),
+  ],
+}
